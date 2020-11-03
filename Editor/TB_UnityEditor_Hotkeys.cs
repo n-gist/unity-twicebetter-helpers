11f645b62b86729350dda0d321639b58a2dd8749
n-gist <58081918+n-gist@users.noreply.github.com>
2020-11-04 00:43:58 +0600

    Add Esc hotkey for deselecting objects

--- Editor/TB_UnityEditor_Hotkeys.cs
+++ Editor/TB_UnityEditor_Hotkeys.cs
@@ -5,7 +5,11 @@ using UnityEditor;
 namespace twicebetter.helpers {
     
     [InitializeOnLoad]
-    public static class TB_UnityEditor_Hotkeys {
+    static class TB_UnityEditor_Hotkeys {
+        static Object[] sceneWindows;
+        static Object[] hierarchyWindows;
+        static System.Type sceneWindowType;
+        static System.Type hierarchyWindowType;
         
         static TB_UnityEditor_Hotkeys() {
             System.Reflection.FieldInfo gehInfo = typeof(EditorApplication).GetField("globalEventHandler", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
@@ -16,7 +20,19 @@ namespace twicebetter.helpers {
         
         static void UnityEditor_Hotkeys_KeyPress() {
             var e = Event.current;
-            if (!(e.control) || e.type != EventType.KeyDown) return;
+            if (e.type != EventType.KeyDown) return;
+            
+            switch (e.keyCode) {
+                case KeyCode.Escape:
+                if (Selection.activeGameObject == null) break;
+                    if (
+                        (FindSceneWindows() && OneOfWindowsIsFocused(sceneWindows)) ||
+                        (FindHierarchyWindow() && OneOfWindowsIsFocused(hierarchyWindows))
+                        ) Selection.objects = null;
+                    break;
+            }
+            
+            if (!(e.control)) return;
             
             switch (e.keyCode) {
                 case KeyCode.F7:
@@ -40,6 +56,26 @@ namespace twicebetter.helpers {
                     Tools.current = Tool.None;
                     break;
             }
+        }
+        static bool OneOfWindowsIsFocused(Object[] windows) {
+            var focusedWindow = EditorWindow.focusedWindow;
+            if (focusedWindow == null) return false;
+            for (int i = 0; i < windows.Length; i++) if (windows[i] == focusedWindow) return true;
+            return false;
+        }
+        static bool FindSceneWindows() {
+            if (sceneWindows != null && sceneWindows.Length > 0 && sceneWindows[0] != null) return true;
+            if (sceneWindowType == null) sceneWindowType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.SceneView");
+
+            sceneWindows = Resources.FindObjectsOfTypeAll(sceneWindowType);
+            return sceneWindows != null;
+        }
+        static bool FindHierarchyWindow() {
+            if (hierarchyWindows != null && hierarchyWindows.Length > 0 && hierarchyWindows[0] != null) return true;
+            if (hierarchyWindowType == null) hierarchyWindowType = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.SceneHierarchyWindow");
+            
+            hierarchyWindows = Resources.FindObjectsOfTypeAll(hierarchyWindowType);
+            return hierarchyWindows != null;
         }
     }
 }
